834717770a5fc5535bd5f8fe812f48d2b209fdd2
shane <1s1carlson@gmail.com>
2016-08-13 12:43:11 +0200

    recommit, after an experimental overhaul

--- gen_nfold.c
+++ gen_nfold.c
@@ -9,8 +9,8 @@
 #include "parameters.h"
 #include "filenames_T0.h"
 
-#include "Potts.h" // !
-//#include "Wolff_Potts_PBC_sq.h" // !
+#include "IM.h" // Potts/Ising
+//#include "Wolff_Potts_PBC_sq.h" // Potts/Ising
 #include "energies_PBC_sq.h"
 #include "domain_size_PBC_sq.h"
 
@@ -19,9 +19,9 @@
 #include "fourn.h"
 #include "autocorrelation_by_FFT.h"
 #include "persistence_corr_FFT.h"
-//#include "correlation_IM_FFT.h" // ! <<<
+#include "correlation_IM_FFT.h" // Potts/Ising
 
-#include "nfold_Potts_heatbath.h" // !
+#include "nfold_IM_Glauber.h" // Potts/Ising
 #include "class_PBC_sq.h"
 #include "nfold.h"
 
@@ -30,6 +30,8 @@
 int main(int argc, char* argv[]){
 
 	time_t start = time(NULL);
+	time_t pstart; //process start time
+	time_t ticstart; //tic start time
 	char name[64]="dir_";
 
 	spins_foldername_T0(name, q, L);
@@ -47,11 +49,15 @@ int main(int argc, char* argv[]){
 	FILE *terminal;
 	terminal=fopen("terminal.txt", "w");
 
-	double QN;
+	FILE *tmr; //for timing tics
+
+	int M; //magnetization
+	double QN; //Q_N - see the original "nfold" paper by Bortz et al
+
+	unsigned long long step_count; //steps taken in a tic
 	double t; //time
 	double tic; //time where data (persistence, domain size) is gathered
 	int t_ind; //indexes the tics
-	int M; //magnetization
 
 	double pic_tic; //actual time when the lattice shots are taken
 	int pic, max_pics=0; //indexes the lattice shots for file handling
@@ -71,14 +77,14 @@ int main(int argc, char* argv[]){
 
 	for(int sim=0; sim<samples; sim++){
 
-		printf("sim %d of %d ", sim+1, samples);
-		fprintf(terminal, "sim %d of %d ", sim+1, samples);
+		printf("sim %d of %d\n", sim+1, samples);
+		fprintf(terminal, "sim %d of %d\n", sim+1, samples);
 
 		initialize_lattice_random_m0(); /*
 
-		initialize_lattice_up_w_prob(0.75); // !
-		P_add=1-exp(-2*B_c_ising); // !
-		for(int swp=0; swp<therm || calculate_magnetization()!=0; swp++){ // !
+		initialize_lattice_up_w_prob(0.75); // Potts/Ising
+		P_add=1-exp(-2*B_c_ising); // Potts/Ising
+		for(int swp=0; swp<therm || calculate_magnetization()!=0; swp++){ // Potts/Ising
 			wolff_step();
 			if(sim==0){
 				calculate_energy();
@@ -88,16 +94,17 @@ int main(int argc, char* argv[]){
 
 		printf("thermalization complete\n");
 		fprintf(terminal, "thermalization complete\n");
-		record_correlation_fn(samples, 0);
-
 		// */
 
+		record_correlation_fn(samples, 0);
+
 		if(sim==0)
-			plot_int_lattice(s, L, 0, 0); // !
+			plot_bool_lattice(s, L, 0, 0); // Potts/Ising
 
 		reset_persistence_lattice();
 		update_all_classes();
 		count_all_members();
+		//create_chains(); // !*
 
 		blocked_state=0;
 		t=0.0;
@@ -105,21 +112,27 @@ int main(int argc, char* argv[]){
 		t_ind=0;
 		pic_tic=1;
 		pic=1;
+		step_count=0;
 
+		ticstart=time(NULL);
 		while(tic<max_time && !blocked_state){
-
 			QN=calculate_QN();
 			t -= log(drand48())/QN; //time update for FOLLOWING step
-
 			if(t > tic){
+				printf("tic %d took %f seconds and %llu steps\n", t_ind, (double)(time(NULL) - ticstart), step_count); //process timer
+
+				tmr=fopen("tictimer.txt", "a"); //---
+				fprintf(tmr, "tic %d took %f seconds and %llu steps\n", t_ind, (double)(time(NULL) - ticstart), step_count); //process timer
+				fclose(tmr); //---
+
 				while(t > tic*tic_mult){
 					printf("tic at %f skipped\n", tic);
 					fprintf(terminal, "tic at %f skipped\n", tic);
 					tic*=tic_mult;
 					t_ind++;
 				}
-				//printf("tic at %3.3f \n", tic);
-				//fprintf(terminal, "tic at %3.3f \n", tic);
+				tmr=fopen("tictimer.txt", "a"); //---
+				pstart=time(NULL); //process timer
 				calculate_energy();
 				fprintf(rts, "%d\t%.20f\t%d\t%.20f\t%.20f\t%.20f\t%.20f\n",
 					t_ind,
@@ -128,29 +141,40 @@ int main(int argc, char* argv[]){
 					calculate_avg_domain_size(),
 					(double)calculate_persistence()/(L*L),
 					(double)members[0]/(L*L),
-					(double)calculate_magnetization(first_top_color())/(L*L) // !
+					(double)calculate_magnetization()/(L*L) // Potts/Ising
 				); //timeseries
+				fprintf(tmr, "\ttic print: %f seconds\n", (double)(time(NULL) - pstart)); //process timer
+				fclose(tmr); //---
+
 				tic*=tic_mult;
 				t_ind++;
+				step_count=0;
+				ticstart=time(NULL);
 			}
 			if(t > pic_tic){
-				//printf("\tpic at %3.3f \n", pic_tic);
-				//fprintf(terminal, "\tpic at %3.3f \n", pic_tic);
 				if(sim==0){
-					plot_int_lattice(s, L, sim, pic); // !
+					tmr=fopen("tictimer.txt", "a"); //---
+					pstart=time(NULL); //process timer
+					plot_bool_lattice(s, L, sim, pic); // Potts/Ising
 					plot_int_lattice(p, L, sim, pic);
 					plot_int_lattice_01(p, L, sim, pic);
+					fprintf(tmr, "\tlat plots: %f seconds\n", (double)(time(NULL) - pstart)); //process timer
+					fclose(tmr); //---
 				}
-				//record_correlation_fn(samples, pic);
+				tmr=fopen("tictimer.txt", "a"); //---
+				record_correlation_fn(samples, pic);
 				record_persistence_corr(samples, pic);
+				fprintf(tmr, "\tcorrelations: %f seconds\n", (double)(time(NULL) - pstart)); //process timer
+				fclose(tmr); //---
 				pic_tic*=pic_tic_mult;
 				pic++;
 			}
 			T0_nfold_step(QN);
+			step_count++;
 		}
 
 		if(blocked_state){
-			M=calculate_magnetization(first_top_color()); // !
+			M=calculate_magnetization(); // Potts/Ising
 			if(M==L*L || M==-L*L){
 				printf("ended at a global minimum at t = %.2f\n", t);
 				fprintf(terminal, "ended at a global minimum at t = %.2f\n", t);
@@ -168,7 +192,7 @@ int main(int argc, char* argv[]){
 		fprintf(rts, "\n\n");
 
 		if(sim==0){
-			plot_int_lattice(s, L, sim, pic); // !
+			plot_bool_lattice(s, L, sim, pic); // Potts/Ising
 			plot_int_lattice(p, L, sim, pic);
 			plot_int_lattice_01(p, L, sim, pic);
 		}
@@ -191,6 +215,7 @@ int main(int argc, char* argv[]){
 		pic_tic*=pic_tic_mult;
 		pic++;
 	}
+
 	fclose(ticsfile);
 
 	printf("process took %.2f seconds\n", (double)(time(NULL) - start));
