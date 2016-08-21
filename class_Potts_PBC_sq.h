cee99b4db17f2509e35a56afd7a0f7b5a7844f97
shane <1s1carlson@gmail.com>
2016-08-22 00:09:45 +0200

    add change to the new Potts class updater, so that the program doesn't have to dereference as much

--- class_Potts_PBC_sq.h
+++ class_Potts_PBC_sq.h
@@ -3,31 +3,32 @@
 //by Shane Carlson
 
 void update_local_classes(int i, int j){
+	int s1=s[i][j]; //the new spin state at i,j
 
-	if( dc[inc(i)][j]&(1<<s[i][j]) ){ //if s[i][j] is newly a color that was dominant at inc(i),j
-		dc[inc(i)][j] = (1<<s[i][j]) + (1<<16); //s[i][j] now the sole dominant color at inc(i),j
-		sc[inc(i)][j] = s[i][j]==s[inc(i)][j] ? 0 : 1; //sc[inc(i)][j] either "stuck" or "must flip"
+	if( dc[inc(i)][j]&(1<<s1) ){ //if s[i][j] is newly a color that was dominant at inc(i),j
+		dc[inc(i)][j] = (1<<s1) + (1<<16); //s[i][j] now the sole dominant color at inc(i),j
+		sc[inc(i)][j] = s1==s[inc(i)][j] ? 0 : 1; //sc[inc(i)][j] either "stuck" or "must flip"
 	}
 	else
 		calculate_class(inc(i), j     );
 
-	if( dc[i][dec(j)]&(1<<s[i][j]) ){
-		dc[i][dec(j)] = (1<<s[i][j]) + (1<<16);
-		sc[i][dec(j)] = s[i][j]==s[i][dec(j)] ? 0 : 1;
+	if( dc[i][dec(j)]&(1<<s1) ){
+		dc[i][dec(j)] = (1<<s1) + (1<<16);
+		sc[i][dec(j)] = s1==s[i][dec(j)] ? 0 : 1;
 	}
 	else
 		calculate_class(i     , dec(j));
 
-	if( dc[dec(i)][j]&(1<<s[i][j]) ){
-		dc[dec(i)][j] = (1<<s[i][j]) + (1<<16);
-		sc[dec(i)][j] = s[i][j]==s[dec(i)][j] ? 0 : 1;
+	if( dc[dec(i)][j]&(1<<s1) ){
+		dc[dec(i)][j] = (1<<s1) + (1<<16);
+		sc[dec(i)][j] = s1==s[dec(i)][j] ? 0 : 1;
 	}
 	else
 		calculate_class(dec(i), j     );
 
-	if( dc[i][inc(j)]&(1<<s[i][j]) ){
-		dc[i][inc(j)] = (1<<s[i][j]) + (1<<16);
-		sc[i][inc(j)] = s[i][j]==s[i][inc(j)] ? 0 : 1;
+	if( dc[i][inc(j)]&(1<<s1) ){
+		dc[i][inc(j)] = (1<<s1) + (1<<16);
+		sc[i][inc(j)] = s1==s[i][inc(j)] ? 0 : 1;
 	}
 	else
 		calculate_class(i     , inc(j));
